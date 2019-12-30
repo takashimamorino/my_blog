4b2d649f1f0bef34ff22f2a0ff1e403b18de6e5d
takashimamorino <does1026@gmail.com>
2019-12-30 22:08:16 +0900

    style

--- src/pages/article/_document.tsx
+++ src/pages/article/_document.tsx
@@ -11,11 +11,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Takashima Katsu</title>
+          <title>TakashimaKatsu</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
           <meta charSet="utf-8" />
           <meta lang="ja" />
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary_large_image" />
+          
+          <meta name='description' content='test'/>
+          <meta property="og:url" content="https://myblog.does1026.now.sh/"/>
+          <meta property="og:site_name" content='TakashimaKatsu'/>
+          <meta property="og:description" content="test"/>
         </Head>
         <body>
         <Main />
